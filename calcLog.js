917bbe9ba3d4e289b0f51f8747d6861c666913c1
Dennis Brown <176719676+Chronous07@users.noreply.github.com>
2024-09-23 18:50:32 -0400

    Finish Calculator Logic
    Complete calculator logic, including button functionality
    Add a "C" or clear button that will only reset the current display
    while maintaining the previously entered number and operator

--- calcLog.js
+++ calcLog.js
@@ -1,38 +1,110 @@
 const calculator = document.getElementById('calculator').querySelector('#container');
 const calcDisplay = calculator.querySelector('#display');
+let number1 = '';
+let number2 = '';
+let operates = '';
+let prbSlv = false;
+let counter = 0;
 
-//A listener event to associate a keyboard reference to a button click.
-//For now just have the listener created and logged in console but later
-//need to update to take the same action as clicking the button with the 
-//mouse
 document.addEventListener('keydown', (e)=> {
-    if(e.key === '0') {console.log(e.key);}
-    if(e.key === '1') {console.log(e.key);}
-    if(e.key === '2') {console.log(e.key);}
-    if(e.key === '3') {console.log(e.key);}
-    if(e.key === '4') {console.log(e.key);}
-    if(e.key === '5') {console.log(e.key);}
-    if(e.key === '6') {console.log(e.key);}
-    if(e.key === '7') {console.log(e.key);}
-    if(e.key === '8') {console.log(e.key);}
-    if(e.key === '9') {console.log(e.key);}
-    if(e.key === '/') {console.log(e.key);}
-    if(e.key === '*') {console.log(e.key);}
-    if(e.key === '%') {console.log(e.key);}
-    if(e.key === '-') {console.log(e.key);}
-    if(e.key === '+') {console.log(e.key);}
-    if(e.key === '.') {console.log(e.key);}
-    if(e.key === 'Delete') {console.log(e.key);}
-    if(e.key === '=' || e.key === 'Enter') {console.log(e.key);}
+    if(e.key == '0') {handleInput('0');}
+    if(e.key == '1') {handleInput('1');}
+    if(e.key == '2') {handleInput('2');}
+    if(e.key == '3') {handleInput('3');}
+    if(e.key == '4') {handleInput('4');}
+    if(e.key == '5') {handleInput('5');}
+    if(e.key == '6') {handleInput('6');}
+    if(e.key == '7') {handleInput('7');}
+    if(e.key == '8') {handleInput('8');}
+    if(e.key == '9') {handleInput('9');}
+    if(e.key === '/') {handleInput('/');}
+    if(e.key === '*') {handleInput('*');}
+    if(e.key === '%') {handleInput('%');}
+    if(e.key === '-') {handleInput('-');}
+    if(e.key === '+') {handleInput('+');}
+    if(e.key === '.') {handleInput('.');}
+    if(e.key === 'Backspace') {handleInput('C');}
+    if(e.key === 'Delete') {handleInput('AC');}
+    if(e.key === '=' || e.key === 'Enter') {handleInput('='); }
 });
 
-calculator.addEventListener('click', (event) =>handleEvent(event));
-
-function handleEvent(event) {
-    let clickedObject = event.target;
-    if (clickedObject.nodeName === 'BUTTON') {
-        calcDisplay.textContent += clickedObject.textContent;
+calculator.addEventListener('click', (event) =>{
+    let trg = event.target
+    if (trg.nodeName === 'BUTTON') {
+            handleInput(trg.textContent);
     }
+});
+
+
+function handleInput(inputValue) {  
+    if (calcDisplay.textContent == 'Try Again') {
+        reset();
+    }
+
+    if (inputValue.match(/[+*/=-]/)){
+        if (inputValue != '=') {operates = inputValue;}
+        if (inputValue == '=' && prbSlv) {return;}
+        if (number1 == '') {
+            number1 = calcDisplay.textContent;
+        } else {
+            if (operates == '') {return;}
+            number2 = calcDisplay.textContent;
+            calcDisplay.textContent = operate(number1, operates, number2);
+            trimDisplay();
+            number1 = '';
+            operates = '';
+            number2 = '';  
+        }
+        return;
+    }
+
+    if (inputValue.match(/[.]/)) {
+        if (calcDisplay.textContent.match(/[.]/)) {return;} 
+    }
+
+    if (inputValue.match(/[%]/)) {
+        calcDisplay.textContent = operate(calcDisplay.textContent, '/', 100);
+        trimDisplay();
+        prbSlv = false;
+        return;
+    }
+
+    if (inputValue == 'C') {
+        calcDisplay.textContent = 0;
+        return;
+    }
+    
+    if (inputValue == 'AC'){
+        reset();
+        return;
+    }
+    
+    if ((calcDisplay.textContent == 0 || number1 == calcDisplay.textContent) || prbSlv == true) {
+        console.log(`num1: ${number1} num2: ${number2}, operator: ${operates} counter: ${counter}`)
+        if(counter >= 2) {
+            calcDisplay.textContent = number1+inputValue;
+            counter = 0;
+            return;
+        }
+        calcDisplay.textContent = inputValue; 
+        counter++;
+        prbSlv = false; 
+        trimDisplay(); 
+    }
+
+    else {calcDisplay.textContent += inputValue; trimDisplay();}
+}
+
+function reset() {
+    calcDisplay.textContent = 0;
+    number1 = '';
+    number2 = '';
+    operates = '';
+    counter = 0;
+}
+
+function trimDisplay() {
+    if (calcDisplay.textContent.length >= 10) {calcDisplay.textContent = calcDisplay.textContent.substring(0,10);}
 }
 
 function add(num1, num2){
@@ -48,20 +120,22 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    if (num2===0){return "Division by zero is not allowed"}
+    if (num2===0){return "Try Again"}
     return (num1 / num2);
 }
 
 function operate(num1, operator, num2){
+    prbSlv = true;
+    counter = 1;
     switch (operator) {
         case '+':
-            return add(num1, num2);
+            return add(Number(num1), Number(num2));
         case '-':
-            return subtract(num1, num2);
+            return subtract(Number(num1), Number(num2));
         case '/':
-            return divide(num1, num2);
+            return divide(Number(num1), Number(num2));
         case '*':
-            return multiply(num1, num2);
+            return multiply(Number(num1), Number(num2));
         default: 
             return `Error, operator ${operator} is not recognized`
     }
